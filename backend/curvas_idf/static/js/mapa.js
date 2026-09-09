46373608f953ac54e0f2dfdb5dbaf207634f2610
maria saavedra <mariapazsaavedra9@gmail.com>
2026-09-09 13:36:22 -0500

    fix: replace CARTO tile layer with OpenStreetMap

--- backend/curvas_idf/static/js/mapa.js
+++ backend/curvas_idf/static/js/mapa.js
@@ -56,10 +56,9 @@ function initMap() {
   });
 
   L.tileLayer(
-    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 19,
     }
   ).addTo(MAP);
